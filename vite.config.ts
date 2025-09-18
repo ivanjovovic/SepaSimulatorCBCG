d83ccc4fa290b96316491c7d5e5f85012d504df2
Ivan <ivanjovovic98@gmail.com>
2025-09-18 15:33:42 +0200

    Deploy i hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()],
-    base: '/sepacbcg/'
- })
+export default defineConfig({
+  plugins: [react()],
+  base: '/SepaSimulatorCBCG/',   // <-- OVO je ispravno za tvoj repo
+})
